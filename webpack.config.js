aab620025f45999f5d458b1e08396121327f1bd8
nmassaro <nicole@massaro.ca>
2017-07-17 22:14:05 -0400

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,27 @@
-const webpack = require('webpack');
-const path = require('path');
+var path = require("path");
 
-const DIST_DIR = path.resolve(__dirname, 'dist');
-const SRC_DIR = path.resolve(__dirname, 'src');
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
 
-const config = {
-  entry: SRC_DIR + '/app/index.js',
-  output: {
-    path: DIST_DIR + '/app',
-    filename: 'bundle.js',
-    publicPath: '/app/'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: [
-            'react',
-            'stage-2'
-          ]
-        }
-      }
-    ]
-  }
+var config = {
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            }
+        ]
+    }
 };
 
 module.exports = config;
