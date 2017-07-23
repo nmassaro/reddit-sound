e54c9ae2194284752512a40d3184dd822e417cbc
nmassaro <nicole@massaro.ca>
2017-07-23 15:05:28 -0400

    Further changes to server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', function(request, response) {
-  response.sendFile('/dist/app/index.html');
+  response.sendFile('index.html');
 });
 
 app.listen(PORT, error => {
